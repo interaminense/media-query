8e25a65cb80a3a4cd38191e16381604270116632
Adriano Interaminense <adriano.interaminense@gmail.com>
2016-04-05 00:48:10 -0300

    adicionando site (trabalho da faculdade)

---
+++ site/src/js/script.js
@@ -0,0 +1,22 @@
+var app = angular.module('app', []);
+var tagName = 'brasilJs';
+
+app.factory('instagram', ['$http', function($http){
+    return {
+        fetchPopular: function(callback){
+            var tagSearch = "https://api.instagram.com/v1/tags/"+tagName+"/media/recent?access_token=194159779.5b9e1e6.23f606c4117f4263a5ee602fa7c98f8d&callback=JSON_CALLBACK";
+            $http.jsonp(tagSearch).success(function(response){
+                callback(response.data);
+            });
+        }
+    }
+}]);
+
+app.controller('mainCtrl', ['$scope', 'instagram', function ($scope, instagram){
+
+    instagram.fetchPopular(function(data){
+        $scope.response = data;
+        console.log($scope.response);
+    });
+
+}]);
