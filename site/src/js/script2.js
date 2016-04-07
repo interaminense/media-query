ad99a5f31580ac4b7bd58409cbfe21c63e7042a0
Adriano Interaminense <adriano.interaminense@gmail.com>
2016-04-07 11:18:28 -0300

    adding improved by tag search system

--- site/src/js/script2.js
+++ site/src/js/script2.js
@@ -3,37 +3,78 @@ var app = angular.module('app', []);
 app.controller('mainCtrl', function ($scope, $http, $window){
 
     $scope.listTag = ['css3'];
+    $scope.urlCur = '';
+    $scope.listResponse = [];
+    $scope.tagSelected = 'css3';
 
-    var _loadJson = function(newTagName) {
-        $http.jsonp("https://api.instagram.com/v1/tags/"+ newTagName +"/media/recent?access_token=194159779.5b9e1e6.23f606c4117f4263a5ee602fa7c98f8d&callback=JSON_CALLBACK").success(function(response) {
+    var _loadJson = function(newTagName, maxTagId) {
+        var url = "https://api.instagram.com/v1/tags/"+ newTagName +"/media/recent?access_token=194159779.5b9e1e6.23f606c4117f4263a5ee602fa7c98f8d&callback=JSON_CALLBACK";
+
+        if(maxTagId != ''){
+            url = "https://api.instagram.com/v1/tags/"+ newTagName +"/media/recent?access_token=194159779.5b9e1e6.23f606c4117f4263a5ee602fa7c98f8d&callback=JSON_CALLBACK&max_tag_id=" + maxTagId;
+        }
+
+        console.log('url', url);
+
+        $http.jsonp(url).success(function(response) {
             $scope.response = response;
+            angular.forEach(response.data, function(p){
+                $scope.listResponse.push(p);
+            });
+            console.log('response', $scope.response);
+            console.log($scope.listResponse);
         }).error(function (response) {
             $scope.response = "Request failed";
         });
     };
 
-    _loadJson('css3');
+    _loadJson('css3', '');
+
+    $scope.nextUrl = function(tagName, maxTagId) {
+        _loadJson(tagName, maxTagId);
+        console.log('tagname', tagName, 'maxTagId', maxTagId);
+    }
 
     $scope.modifyTagName = function(newTagName) {
-        _loadJson(newTagName);
+        $scope.listResponse = [];
+        _loadJson(newTagName, '');
         $scope.listTag.push(newTagName);
+        $scope.tagSelected = newTagName;
     }
 
     $scope.submit = function() {
+        $scope.listResponse = [];
         if ($scope.tagName) {
-            _loadJson($scope.tagName);
+            _loadJson($scope.tagName, '');
             $scope.listTag.push($scope.tagName);
+            $scope.tagSelected = $scope.tagName;
             $scope.tagName = '';
         }
     };
 
+    $scope.showBigPhoto = function(e) {
+        console.log(e);
+        e.srcElement.parentNode.classList.toggle('active');
+    }
+
+    $scope.order = function(predicate) {
+        $scope.predicate = predicate;
+        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+        $scope.friends = orderBy($scope.friends, predicate, $scope.reverse);
+    };
+
     angular.element($window).bind("scroll", function() {
-        var positionScroll = document.documentElement.scrollTop;
         var header = document.querySelector('.header');
-        if(positionScroll > 100){
+        var mini = document.querySelector('.mini');
+        if(this.pageYOffset >= 100){
             header.classList.add('active');
         }else{
             header.classList.remove('active');
+        }
+        if(this.pageYOffset >= 200){
+            mini.classList.add('active');
+        }else{
+            mini.classList.remove('active');mini
         }
     });
 
